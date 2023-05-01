579982e37a0accae07b86d1eda43b11ac52ae4aa
Elijah Molloy <elijahmolloy@gmail.com>
2023-05-01 08:02:50 -0600

    creation of token type enum. Added all auth controller methods with exception throwing

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,44 @@
-import { Controller } from '@nestjs/common';
+import { Controller, NotImplementedException, Post } from '@nestjs/common';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    @Post('register')
+    async register(): Promise<any> {
+        throw new NotImplementedException();
+    }
+
+    @Post('login')
+    async login(): Promise<any> {
+        throw new NotImplementedException();
+    }
+
+    @Post('logout')
+    async logout(): Promise<any> {
+        throw new NotImplementedException();
+    }
+
+    @Post('refresh-tokens')
+    async refreshTokens(): Promise<any> {
+        throw new NotImplementedException();
+    }
+
+    @Post('forgot-password')
+    async forgotPassword(): Promise<any> {
+        throw new NotImplementedException();
+    }
+
+    @Post('reset-password')
+    async resetPassword(): Promise<any> {
+        throw new NotImplementedException();
+    }
+
+    @Post('send-verification-email')
+    async sendVerificationEmail(): Promise<any> {
+        throw new NotImplementedException();
+    }
+
+    @Post('verify-email')
+    async verifyEmail(): Promise<any> {
+        throw new NotImplementedException();
+    }
+}
