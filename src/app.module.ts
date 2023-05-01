092614836b69a411db2850a7ba3ec7af5838eba4
Elijah Molloy <elijahmolloy@gmail.com>
2023-05-01 07:31:35 -0600

    added config support

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { TokensModule } from './tokens/tokens.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule, AuthModule, TokensModule],
+  imports: [UsersModule, AuthModule, TokensModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
