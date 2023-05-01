30ee41de163b391e869586552ffd8ea7e3405f43
Elijah Molloy <elijahmolloy@gmail.com>
2023-05-01 07:33:41 -0600

    added mongoDB support

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { TokensModule } from './tokens/tokens.module';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UsersModule, AuthModule, TokensModule, ConfigModule.forRoot()],
+  imports: [UsersModule, AuthModule, TokensModule, ConfigModule.forRoot(), MongooseModule.forRoot(process.env.MONGODB_URL)],
   controllers: [AppController],
   providers: [AppService],
 })
