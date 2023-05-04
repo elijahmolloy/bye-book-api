d48ac40c87a04faeb0eec18f03218722006b19c3
Elijah Molloy <elijahmolloy@gmail.com>
2023-05-03 19:22:57 -0600

    added client image resource

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -6,15 +6,21 @@ import Client from 'mailgun.js/client';
 
 @Injectable()
 export class EmailService {
-	// private readonly mailgunClient: Client;
-	// constructor(private readonly configService: ConfigService) {
-	//     const mailgun = new Mailgun(formData);
-	//     this.mailgunClient = mailgun.client({
-	//         username: 'api',
-	//         key: configService.get('MAILGUN_PRIVATE_API_KEY')
-	//     });
-	// }
-	// async sendVerificationEmail(): Promise<boolean> {
-	//     throw new NotImplementedException();
-	// }
+	private readonly mailgunClient: Client;
+
+	constructor(configService: ConfigService) {
+	    const mailgun = new Mailgun(formData);
+	    this.mailgunClient = mailgun.client({
+	        username: 'api',
+	        key: configService.get('MAILGUN_PRIVATE_API_KEY')
+	    });
+	}
+
+	async sendVerificationEmail() {
+	    throw new NotImplementedException();
+	}
+
+	async sendResetPasswordEmail() {
+		throw new NotImplementedException();
+	}
 }
